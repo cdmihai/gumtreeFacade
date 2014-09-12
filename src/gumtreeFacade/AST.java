31e3aaaee26a925db694a4eb10c6ee1a74feed47
Mihai Codoban <codobanmihai@gmail.com>
2014-09-12 10:33:56 -0700

    remove file write side effect and return json string

--- src/gumtreeFacade/AST.java
+++ src/gumtreeFacade/AST.java
@@ -1,16 +1,26 @@
 package gumtreeFacade;
 
-import fr.labri.gumtree.client.batch.JSONProcessor;
+import java.io.IOException;
+
+import fr.labri.gumtree.client.TreeGeneratorRegistry;
+import fr.labri.gumtree.io.TreeIoUtils;
+import fr.labri.gumtree.tree.Tree;
 
 public class AST {
 	
 	
-	public static void generateASTs(String inputDirectory) {
-		JSONProcessor jsGenerator = new JSONProcessor(inputDirectory);
-		jsGenerator.process();
+	public static String getASTinJSON(String filePath) throws IOException {
+		String json = null;
+
+		Tree t = TreeGeneratorRegistry.getInstance().getTree(filePath);
+		if (t != null) {
+			json = TreeIoUtils.toJSON(t);
+		}
+		
+		return json;
 	}
 	
-	public static void main(String[] args) {
-		generateASTs("/Users/mihai/work/gumtree/gumtreeFacade/src/gumtreeFacade");
+	public static void main(String[] args) throws IOException {
+		System.out.println(getASTinJSON("/Users/mihai/work/gumtree/gumtreeFacade/src/gumtreeFacade/AST.java")); 
 	}
 }
